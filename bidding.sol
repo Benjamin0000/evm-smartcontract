29f3d671901a7a40b73684956222fff6b2a6b532
Benjamin Odiowa <benco00@yahoo.com>
2022-02-10 12:41:29 +0100

    initial commit

--- bidding.sol
+++ bidding.sol
@@ -9,41 +9,76 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 contract Bidding is ERC1155Holder, ERC721Holder{
 
     struct Item{
-        string title;
+        string name;
+        string description;
+        string url;
         uint id; //nft id
         address Address; // contract
         uint8 standard; //erc1155 || 721
-        uint startTime; 
-        uint price;
+        uint startTime;
+        uint price; //actual price
         uint startPrice;
-        bool status; // 0||1 
+        bool status; // 0||1
         mapping(address=>uint) bidPoints;
         address[] bidders;
         uint points;
+        address winner;
     }
-    
-    uint16 public sec;
-    uint public pointPrice; // set in ether
-    address cnoteTokenAddress;
-    address minPass;
-    address admin = address(0x1Fd793c451653C26c94185bC5d5b43a2E4a2e797);
 
+    // used to get biddings data
+    struct UserBidData{
+        uint points;
+        bool eligible;
+        uint time;
+        uint bonus;
+        uint lodged;
+    }
+
+    uint total;
+
+    uint public bonusPoint = 100;
+    uint16 public sec = 15;
+    uint public pointPrice; // set in ether
+    address cnoteToken;
+    address mintPass = address(0x4240898E9db56FF78B4fFA7006Af0c6Ec59D9Ec5);
+    //address admin = address(0x1Fd793c451653C26c94185bC5d5b43a2E4a2e797);
+    address admin = address(0x93EC829Ca2Eb2d49Db55Cb6799189f6f2D3C1DC1);
     mapping(address=>uint) public points;
     mapping(uint=>Item) public items;
 
-    constructor() {
-        sec = 15;
+    modifier onlyOwner {
+        require(msg.sender == admin);
+        _;
     }
 
-    function listItem() public {
-
+    function listItem(
+        string memory name,
+        string memory description,
+        string memory url,
+        uint _id,
+        address _address,
+        uint8 standard,
+        uint startTime,
+        uint price,
+        uint startPrice
+    ) public onlyOwner {
+        items[total].name = name;
+        items[total].description = description;
+        items[total].url = url;
+        items[total].id = _id;
+        items[total].Address = _address;
+        items[total].standard = standard;
+        items[total].startTime = block.timestamp + (startTime  * 3600);
+        items[total].startPrice = startPrice;
+        items[total].price = price;
+        total+1;
     }
-    
+
     //before transfer make sure user approves dapp
     function buyPoint(uint _points) public {
-        require(IERC721(minPass).balanceOf(msg.sender) > 0, "Get the mint pass");
+        require(IERC721(mintPass).balanceOf(msg.sender) > 0, "Get the mint pass");
         uint cost = _points * pointPrice;
-        IERC20(cnoteTokenAddress).transferFrom(msg.sender, address(this), cost);
+        IERC20(cnoteToken).transferFrom(msg.sender, address(this), cost);
         unchecked{
             points[msg.sender] += _points;
         }
@@ -51,31 +86,61 @@ contract Bidding is ERC1155Holder, ERC721Holder{
 
     function placeBid(uint id, uint _points) public {
         Item storage item = items[id];
+        uint balance = points[msg.sender];
+        bool hasPass = canPlaceBid(id).eligible;
+        uint bidded = item.bidPoints[msg.sender]; //already alotted point
+        bool letGo = bidded < bonusPoint && hasPass; //new bidder
+
         require( !item.status, "Bidding ended");
         require( block.timestamp <= item.startTime, "Bidding already started");
-        require( points[msg.sender] >= _points, "Insuficient for bid");
+
+        if( letGo ){
+            unchecked{
+                balance += (bonusPoint - bidded);
+            }
+        }else{
+            require( hasPass, "You are not eligible");
+        }
+
+        require( balance >= _points, "Insufficient for bid");
 
         unchecked{
-            points[msg.sender] -= _points;
+            uint point2 = _points;
+
+            if( letGo ){
+                uint result = (bonusPoint - bidded);
+                if(result <= point2)
+                    point2 -= result;
+            }
+
+            if( points[msg.sender] >= point2 )
+                points[msg.sender] -= point2; //reducing users point
+
             item.points += _points;
         }
+
         item.bidders.push(msg.sender);
-        item.bidPoints[msg.sender] = _points;
+        unchecked{
+            item.bidPoints[msg.sender] += _points;
+        }
     }
 
     function claimPrice(uint id) public {
         Item storage item = items[id];
-        require( item.bidPoints[msg.sender] > 0, "You are not in this contest" ); 
+        require( item.bidPoints[msg.sender] > 0, "You are not in this contest" );
         require( block.timestamp > item.startTime, "Bidding has not started" );
         require( !item.status, "Already claimed");
         require( bidEnded(id), "Bidding has not ended");
-        
+        require( canPlaceBid(id).eligible, "You are not eligible");
+
         if( msg.sender == getWinner(id) ){
             if(item.standard == 1)
                 sendERC721(item, msg.sender);
             else
                 sendERC1155(item, msg.sender);
         }
+
+        item.winner = msg.sender;
     }
 
     function getWinner(uint id) private view returns(address) {
@@ -83,6 +148,7 @@ contract Bidding is ERC1155Holder, ERC721Holder{
         uint bids = 0;
         address winner = address(0);
         for(uint i = 0; i < item.bidders.length; i++){
+
             uint _points = item.bidPoints[ item.bidders[i] ];
             if( bids < _points){
                 bids = _points;
@@ -90,6 +156,19 @@ contract Bidding is ERC1155Holder, ERC721Holder{
             }
         }
         return winner;
+    }
+
+    function canPlaceBid(uint _id) public view returns(UserBidData memory){
+        bool eligible = false;
+        if(IERC721(mintPass).balanceOf(msg.sender) > 0) eligible = true;
+
+        return UserBidData({
+            points: points[msg.sender],
+            eligible: eligible,
+            time:items[_id].startTime,
+            bonus:bonusPoint,
+            lodged:items[_id].bidPoints[msg.sender]
+        });
     }
 
     function bidEnded(uint id) public view returns(bool){
@@ -100,7 +179,7 @@ contract Bidding is ERC1155Holder, ERC721Holder{
 
     function sendERC721(Item storage item, address to) private {
         IERC721(item.Address).transferFrom(address(this), to, item.id);
-        item.status = true; 
+        item.status = true;
     }
 
     function sendERC1155(Item storage item, address to) private {
@@ -108,14 +187,35 @@ contract Bidding is ERC1155Holder, ERC721Holder{
         item.status = true;
     }
 
-    function changeSeconds(uint16 _sec) public {
-        require(msg.sender == admin, "Unathorized");
+    function changeSeconds(uint16 _sec) public onlyOwner{
         sec = _sec;
     }
 
-    function changePointPrice(uint _price) public {
-        require(msg.sender == admin, "Unathorized");
+    function changePointPrice(uint _price) public onlyOwner {
         pointPrice = _price;
     }
-    
+
+    function changeTokenAddress(address _address) public onlyOwner {
+        cnoteToken = _address;
+    }
+
+    function changeMintPassAddress(address _address) public onlyOwner {
+        mintPass = _address;
+    }
+
+    function moveCnotToken(uint amt) public onlyOwner {
+        IERC20(cnoteToken).transferFrom(address(this), admin, amt);
+    }
+
+    function moveERC721(address _address, uint _id) public onlyOwner{
+        IERC721(_address).transferFrom(address(this), admin, _id);
+    }
+
+    function moveERC1155( address _address, uint[] memory _id, uint[] memory _amt) public onlyOwner {
+        IERC1155(_address).safeBatchTransferFrom(address(this), admin, _id, _amt, "");
+    }
+
+    function setBonusPoint(uint _points) public onlyOwner{
+        bonusPoint = _points;
+    }
 }
--- nft_pass.sol
+++ nft_pass.sol
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.0; 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
+pragma solidity ^0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-
-contract CnoteMintPass is  ERC721URIStorage{ 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+contract CnoteMintPass is  ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("Cnot mint pass", "CNOT MINT PASS") {
-        owner = address(0x1Fd793c451653C26c94185bC5d5b43a2E4a2e797);
-        _tokenURL = "https://gateway.pinata.cloud/ipfs/QmcwBHmr6XU5QcBYL35UsEPnSQBJmzz28erxvMmmmmmy6J";
+    constructor() ERC721("Cnote mint pass", "CNOTE MINT PASS") {
+        //_owner = address(0x1Fd793c451653C26c94185bC5d5b43a2E4a2e797);
+        _owner = msg.sender;
+       // _tokenURLS = "https://gateway.pinata.cloud/ipfs/QmcwBHmr6XU5QcBYL35UsEPnSQBJmzz28erxvMmmmmmy6J";
         _contractURL = "https://gateway.pinata.cloud/ipfs/QmcKGQTYRQNMZA5LYaJtiP7u6dMZfpEERXWdRmYeqpzpxZ";
     }
 
@@ -18,33 +18,72 @@ contract CnoteMintPass is  ERC721URIStorage{
     uint256 public price = 0.001 ether;
     uint256 public maxMint = 5;
     uint256 public totalSupply;
+    uint16  public lastMeta = 0;
 
-    string public _tokenURL;
+    string[] _tokenURLS = [  //set the five metadata here.
+        "first",
+        "second",
+        "third",
+        "forth",
+        "fifth"
+    ];
+
+    mapping(uint256 => string) private _tokenURIs;
+    mapping(uint16=>uint) public metaTracker;
+    mapping(address=>bool) public whiteListed;
+
+
+    bool public stopWhiteList;
+    bool private dontShowURL;
     string private _contractURL;
-    address public owner;
-   
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
-    }
+    string private hiddenURL = ""; // set the hidden url here
+    address private _owner;
+
 
     function mintNFT(uint256 amt) public payable returns (uint256){
+        require(balanceOf(msg.sender) < maxMint, "You can no longer mint");
         uint256 newid = _tokenIds.current();
         require(amt > 0 && amt <= maxMint, "too much minting");
         uint256 totalID = (newid + amt) - 1;
         require( totalID <= MAX, "max mint cap reached");
         require( msg.value == price * amt, "Under priced");
+        if(!stopWhiteList)
+            require(whiteListed[msg.sender], "You've not been whitelisted");
 
         for(uint256 i = 1; i <= amt; i++){
             newid = _tokenIds.current();
             _mint(msg.sender, newid);
-            _setTokenURI(newid, _tokenURL);
+            _setTokenURI(newid, _tokenURLS[lastMeta]);
             _tokenIds.increment();
+            metaTracker[lastMeta]++;
+            if(lastMeta < 4){
+                // if(lastMeta == 0 && metaTracker[lastMeta] >= 10)
+                     lastMeta++;
+                // if(lastMeta == 1 && metaTracker[lastMeta] >= 10){
+                //     lastMeta++;
+                // }
+            }else{
+                lastMeta = 0;
+            }
         }
         totalSupply = _tokenIds.current();
-        payable(owner).transfer(msg.value); 
+        payable(_owner).transfer(msg.value);
         return newid;
     }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        if(dontShowURL) return hiddenURL;
+        return _tokenURIs[tokenId];
+    }
+
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) override internal virtual {
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
+    function owner() public override view returns (address) {
+        return _owner;
+    }
+
 
     function burn(uint256 tokenID) public {
         _burn(tokenID);
@@ -59,8 +98,10 @@ contract CnoteMintPass is  ERC721URIStorage{
         MAX = _MAX; price = _price; maxMint = _maxMint;
     }
 
-    function updateTokenUrl(string memory _url) public onlyOwner {
-        _tokenURL = _url;
+    function updateTokenUrl(string[] memory _url) public onlyOwner {
+        for(uint i = 0; i < _url.length; i++){
+            _tokenURLS[i] = _url[i];
+        }
     }
 
     function updateContractUrl(string memory _url) public onlyOwner {
@@ -69,6 +110,24 @@ contract CnoteMintPass is  ERC721URIStorage{
 
     function contractURI() public view returns (string memory){
         return _contractURL;
+    }
+
+    function setWhiteList(bool status) public onlyOwner {
+        stopWhiteList = status;
+    }
+
+    function hideURL(bool status) public onlyOwner {
+        dontShowURL = status;
+    }
+
+    function addToWhiteList(address[] memory _users, uint8 _type) public onlyOwner {
+        if(_type == 1){
+            for(uint i = 0; i < _users.length; i++)
+                whiteListed[_users[i]] = true;
+        }else{
+            for(uint i = 0; i < _users.length; i++)
+                whiteListed[_users[i]] = false;
+        }
     }
 
 }
